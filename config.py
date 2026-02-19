f2cffdc839cff04e896c6ea4152a36a4cd03d5c8
Lalit-Mahajan <139694592+Lalit-Mahajan@users.noreply.github.com>
2026-02-19 15:01:15 +0530

    linkedin all  page commit

--- config.py
+++ config.py
@@ -1,10 +1,27 @@
-MONGO_URI = "mongodb+srv://wtwinds26_db_user:3X1Ck3momZJLg6pp@cluster0.wkjccr9.mongodb.net/wtwinds"
+import os
+from dotenv import load_dotenv
 
-SECRET_KEY = "wtwinds_secret"
+load_dotenv()
 
-MAIL_SERVER = "smtp.gmail.com"
-MAIL_PORT = 587
-MAIL_USE_TLS = True
+MONGO_URI = os.getenv("MONGO_URI")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-MAIL_USERNAME = "lalitmahajan.wtwinds@gmail.com"   # your real gmail
-MAIL_PASSWORD = "ojigjyfshhpdawwd"    
+MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
+
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+
+
+# MONGO_URI = "mongodb+srv://wtwinds26_db_user:3X1Ck3momZJLg6pp@cluster0.wkjccr9.mongodb.net/wtwinds"
+
+# SECRET_KEY = "wtwinds_secret"
+
+# MAIL_SERVER = "smtp.gmail.com"
+# MAIL_PORT = 587
+# MAIL_USE_TLS = True
+
+# MAIL_USERNAME = "lalitmahajan.wtwinds@gmail.com"   
+# MAIL_PASSWORD = "usjtphtsrojltotd"    
